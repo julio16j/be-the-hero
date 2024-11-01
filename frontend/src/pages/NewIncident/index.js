f2f718557c4c8477a60c67947ed12b013ffce838
Julio Cesar <jc.urbano.vasconcelos@gmail.com>
2024-10-31 22:24:56 -0300

    printscreen for create a case page

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -35,21 +35,21 @@ export default function NewIncident() {
     <div className="new-incident-container">
       <div className="content">
         <section>
-          <img src={logoImg} alt="Be The Hero"/>
-          <h1>Cadastrar novo caso</h1>
-          <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso. </p>
+          <img src={logoImg} alt="Be The Hero" />
+          <h1>Create a new case</h1>
+          <p>Describe the case in detail to find a hero to solve it.</p>
 
-          <Link className = "back-link"   to="/profile">
-            <FiArrowLeft  size={16} color ="#E02041" />
-            Voltar para home
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Back to home
           </Link>
         </section>
-        <form onSubmit={handleCreateIncident} >
-          <input placeholder ="Título do caso" value={title} onChange={e => setTitle(e.target.value)} />
-          <textarea placeholder ="Descrição" value={description} onChange={e => setDescription(e.target.value)} />
-          <input  placeholder="Valor em reais" value={value} onChange={e => setValue(e.target.value)} />
+        <form onSubmit={handleCreateIncident}>
+          <input placeholder="Case title" value={title} onChange={e => setTitle(e.target.value)} />
+          <textarea placeholder="Description" value={description} onChange={e => setDescription(e.target.value)} />
+          <input placeholder="Amount" value={value} onChange={e => setValue(e.target.value)} />
 
-          <button className="button" type="submit" >Cadastrar</button>
+          <button className="button" type="submit">Register</button>
         </form>
       </div>
     </div>
